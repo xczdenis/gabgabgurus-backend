94bd59896b22e9a4d9fdd2b3e5101bf7797d9709
Denis <xczdenis@gmail.com>
2023-12-06 11:28:31 +0300

    refactor: a little refactoring

--- src/gabgabgurus/config/settings/components/rest_auth.py
+++ src/gabgabgurus/config/settings/components/rest_auth.py
@@ -1,12 +1,10 @@
-from django.conf import settings
-
 ACCESS_TOKEN_NAME = "access"  # noqa: S105 Possible hardcoded password
 REFRESH_TOKEN_NAME = "refresh"  # noqa: S105 Possible hardcoded password
 
 REST_AUTH = {
     "TOKEN_MODEL": None,
     "USE_JWT": True,
-    "JWT_AUTH_SECURE": settings.DEBUG,
+    "JWT_AUTH_SECURE": True,
     "JWT_AUTH_COOKIE": ACCESS_TOKEN_NAME,
     "JWT_AUTH_REFRESH_COOKIE": REFRESH_TOKEN_NAME,
 }
