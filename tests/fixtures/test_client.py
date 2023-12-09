0ef9f599e69b01c175502df4a036f4fbac1c5505
Denis <xczdenis@gmail.com>
2023-12-09 19:52:00 +0300

    feat: add tests in docker

--- tests/fixtures/test_client.py
+++
@@ -1,8 +0,0 @@
-# import pytest
-#
-# from tests.utils.api_client import APIClient
-#
-#
-# @pytest.fixture(scope="session")
-# def test_client(test_app):
-#     return APIClient(test_client=test_app.test_client())
